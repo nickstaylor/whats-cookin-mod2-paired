1a82c47be01019180ce58f8ac99e452b34f4ac82
Taras Tarlov <ttarlov@gmail.com>
2020-03-25 15:47:24 -0600

    remove unused data

--- src/pantry.js
+++ src/pantry.js
@@ -1,31 +1,5 @@
 // let rawIngredientsData = ingredientsData !== undefined ? ingredientsData : require('../data/ingredients')
-let listOfIngredients = [
-  {
-  "id": 11477,
-  "name": "wheat flour",
-  "estimatedCostInCents": 142
-  },
-  {
-  "id": 18372,
-  "name": "bicarbonate of soda",
-  "estimatedCostInCents": 582
-  },
-  {
-  "id": 20081,
-  "name": "eggs",
-  "estimatedCostInCents": 472
-  },
-  {
-  "id": 19335,
-  "name": "sucrose",
-  "estimatedCostInCents": 902
-  },
-  {
-  "id": 19206,
-  "name": "instant vanilla pudding",
-  "estimatedCostInCents": 660
-  }
-];
+
 let rawIngredientsData = require('../data/ingredients')
 
 class Pantry {
