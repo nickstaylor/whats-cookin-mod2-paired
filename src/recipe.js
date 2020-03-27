9a98db7074397ada3ded312e826f4e5b70ebe895
Nick Taylor <nickstaylor@gmail.com>
2020-03-26 19:43:25 -0600

    add generate full ingredients method to recipe.js

--- src/recipe.js
+++ src/recipe.js
@@ -1,13 +1,44 @@
+let rawIngredientsData = require('../data/ingredients')
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
-    this.image = recipe.image;
-    this.ingredients = recipe.ingredients;
+    this.image = recipe.image
+    this.ingredients = this.generateFullIngredientList(recipe.ingredients)
     this.instructions = recipe.instructions;
     this.name = recipe.name;
     this.tags = recipe.tags;
     this.isFavorited = false;
   }
+
+  calculateCostOfIngredientsNeeded(){
+  // takes all new ingredients that were needed to complete a recipe and calculates the total cost
+  }
+
+  generateFullIngredientList(partialIngredients) {
+    return partialIngredients.map(ingredient => {
+      let matchedIngredient = rawIngredientsData.find(rawIngredient =>{
+      return ingredient.id === rawIngredient.id
+      })
+      return {name:matchedIngredient.name,
+              id: ingredient.id,
+              amount: ingredient.quantity.amount,
+              unit: ingredient.quantity.unit,
+              estimatedCostInCents: matchedIngredient.estimatedCostInCents
+            };
+
+      })
+    }
+
+
+
+
+
+
+
+
+
+
 }
 
 module.exports = Recipe;
