1fa38e956c35241c5789b8f284cce343d79a6a11
Nick Taylor <nickstaylor@gmail.com>
2020-03-27 17:41:24 -0600

    add initial event listeners and global variables.  Also attempt to connect files at top

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,21 @@
 console.log('Hello world');
+
+let recipeSection = document.querySelector('.recipe-container');
+let randomNumber = ((Math.ceil(Math.random() * 50)));
+const user = new User(usersData[randomNumber], ingredientsData)
+console.log(user);
+//
+// recipeSection.addEventListener('click', displayRecipes);
+//
+//
+//
+// function instantiateUser(){
+//
+//   //random function that chooses user
+//   //49 users
+//
+// }
+//
+// function displayAllRecipes(){
+//
+// }
