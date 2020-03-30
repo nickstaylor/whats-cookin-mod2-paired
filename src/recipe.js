6d0cb55c62ebcfc8b0a2027748eb5fcfa10fc05e
Taras Tarlov <ttarlov@gmail.com>
2020-03-29 20:09:37 -0600

    add require statements for tests to work in node and browser

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,12 @@
+let rawIngredientsData;
+
+if(typeof module !== 'undefined') {
+  rawIngredientsData = require('../data/ingredients')
+} else {
+  rawIngredientsData = ingredientsData
+}
+
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -16,7 +25,7 @@ class Recipe {
 
   generateFullIngredientList(partialIngredients) {
     return partialIngredients.map(ingredient => {
-      let matchedIngredient = ingredientsData.find(rawIngredient =>{
+      let matchedIngredient = rawIngredientsData.find(rawIngredient =>{
       return ingredient.id === rawIngredient.id
       })
       return {name:matchedIngredient.name,
